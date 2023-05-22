4a3f2ea54eeafb219fb9dfb0392eae15fecb2467
EunSu <eunning09005@gmail.com>
2023-05-22 20:04:47 +0900

    [Feature] Test for Eunsu
    Test for Eunsu

--- Prototype-Pummel Party/Assets/Scripts/Tile.cs
+++ Prototype-Pummel Party/Assets/Scripts/Tile.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
     void Start()
     {
-        
+        Debug.Log("Eunsu");
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
