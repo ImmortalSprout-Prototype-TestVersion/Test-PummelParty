ed7de5f0a54b372ccfacecf0306c109d8374c181
rickythediver <wooseok1995@gmail.com>
2023-05-30 23:56:53 +0900

    [Add] RoomList, RoomData 스크립트 작성 + UI 프리팹화 + 임시 나가기 버튼 만듦
    1. RoomList, RoomData 스크립트 작성
    2. UI 프리팹화(RoomList, RoomButton)
    3. 임시 나가기 버튼 생성

---
+++ Prototype-Pummel Party/Assets/Scripts/RoomData.cs
@@ -0,0 +1,74 @@
+using Photon.Pun.Demo.Cockpit;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoomData : MonoBehaviour
+{
+    [SerializeField] private TMP_Text roomButtonText;
+    [SerializeField] private Image roomButtonBackground;
+    private RoomInfo information;
+    public string roomName { get; private set; }
+
+    private Color defaultColor = Color.white;
+    private Color selectedColor = Color.green;
+
+    private RoomList rooms;
+
+    private void Start()
+    {
+        rooms = transform.GetComponentInParent<RoomList>();
+    }
+    public void SetRoomInfo(RoomInfo roomInformation)
+    {
+        information = roomInformation;
+        roomName = roomInformation.Name;
+    }
+
+    public void SetRoomText(string roomName, int currentPlayerCount, int maxPlayerCount)
+    {
+        roomButtonText.text = roomName + $" ({currentPlayerCount} / {maxPlayerCount})";
+    }
+
+    public void SelectRoom()
+    {
+        if (rooms.GetSelectedRoom() == null) // 이미 선택된 방이 없다면
+        {
+            rooms.SetSelectedRoom(this); // 자기 자신을 선택된 방으로 만든다
+            SetColorSelected(); // 자기자신을 선택된 색깔로 칠한다
+        }
+        else // 이미 선택된 방이 있다면
+        {
+            if (rooms.GetSelectedRoom() == this) // 근데 그 방이 지 자신이라면
+            {
+                rooms.SetRoomNull(); // 선택된 방을 null 로 만든다
+                SetColorDefault(); // 자기자신을 디폴트 색으로 칠한다
+            }
+            else // 근데 그게 다른 방이라면
+            {
+                rooms.GetSelectedRoom().SetColorDefault(); // 다른 방의 색을 디폴트색으로 칠한다
+                rooms.SetSelectedRoom(this); // 지 자신을 선택된 방으로 만든다
+                SetColorSelected(); // 자신을 석탠된 색깔로 칠한다
+            }
+        }
+    }
+
+    /// <summary>
+    /// 방버튼의 배경색을 디폴트 색으로 칠한다
+    /// </summary>
+    public void SetColorDefault()
+    {
+        roomButtonBackground.color = defaultColor;
+    }
+
+    /// <summary>
+    /// 방 버튼의 배경색을 선택한 색으로 칠한다
+    /// </summary>
+    public void SetColorSelected()
+    {
+        roomButtonBackground.color = selectedColor;
+    }
+}
---
+++ Prototype-Pummel Party/Assets/Scripts/RoomList.cs
@@ -0,0 +1,67 @@
+using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomList : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private RoomData roomData;
+    [SerializeField] private Transform roomListPosition;
+
+    private Dictionary<string, RoomData> rooms= new Dictionary<string, RoomData>();
+
+    [SerializeField] private RoomData selectedRoom;
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        foreach (RoomInfo roomInfo in roomList)
+        {
+            if (roomInfo.RemovedFromList) // 방이 삭제된 것이라면
+            {
+                if (rooms.ContainsKey(roomInfo.Name)) // 딕셔너리에 등록된 방이라면
+                {
+                    Destroy(rooms[roomInfo.Name].gameObject); // UI상에서 방을 삭제한다
+                    rooms.Remove(roomInfo.Name); // 딕셔너리에서 방의 Key를 삭제한다
+                }
+            }
+            else // 삭제가 아닌 변경사항이 발생했다면
+            {
+                RoomData newRoom = null; // 새로운 UI 방을 저장할 변수입니다
+
+                if (rooms.ContainsKey(roomInfo.Name)) // 해당 방이 이미 딕셔너리에 존재한다면
+                {
+                    return; // 아무것도 안하고 함수를 종료한다
+                }
+
+                newRoom = Instantiate(roomData, roomListPosition); 
+                // roomListPosition을 부모로 하는 roomData를 생성해준다
+
+                rooms.Add(roomInfo.Name, newRoom); // 딕셔너리에 새 방을 넣어준다
+
+                newRoom.SetRoomInfo(roomInfo); // newRoom의 information에 roomInfo를 할당한다
+                newRoom.SetRoomText(roomInfo.Name, roomInfo.PlayerCount, roomInfo.MaxPlayers); // 방이 표시해야할 텍스트를 입력해준다
+            }
+        }
+    }
+
+    public RoomData GetSelectedRoom()
+    {
+        return selectedRoom;
+    }
+
+    public void SetSelectedRoom(RoomData _selectedRoom)
+    {
+        selectedRoom = _selectedRoom;
+    }
+
+    public void SetRoomNull()
+    {
+        selectedRoom = null;
+    }
+
+    public Dictionary<string, RoomData> GetRoomList()
+    {
+        return rooms;
+    }
+}
