5de49a43d80168c2983026580155a0892108594b
rickythediver <wooseok1995@gmail.com>
2023-05-30 13:33:05 +0900

    [Modify] Goal 스크립트 수정
    OncollisionEnter에서 OnTriggerEnter로 수정하였음

--- Prototype-Pummel Party/Assets/Scripts/Goal.cs
+++ Prototype-Pummel Party/Assets/Scripts/Goal.cs
@@ -6,28 +6,33 @@ using UnityEngine;
 public class Goal : MonoBehaviour
 {
     private float time;
+    private bool stopTimer;
 
     private void Update()
     {
-        time += Time.deltaTime;   
-    }
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (!collision.gameObject.CompareTag("Player"))
+        if (stopTimer == false)
         {
-            return;
+            time += Time.deltaTime;
         }
 
-        /*--------------------------------------------------------
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+         /*--------------------------------------------------------
          * 무언가의 통계처리?
          * 멀티라면 등수 확보
          * 확보한 등수를 보드게임 신, 미니게임 결과 신으로 전달
          * 
          * 보드게임 로드 신
-        */
+         */
+            stopTimer = true;
+            Debug.Log("와 ! 도착 1등 보드게임으로 돌아가요");
+            Debug.Log($"걸린 시간 : {time}");
+            Time.timeScale = 0f;
+        }
 
-        Debug.Log("와 ! 도착 1등 보드게임으로 돌아가요");
-        Debug.Log($"걸린 시간 : {time}");
-        Time.timeScale = 0f;
+
     }
 }
