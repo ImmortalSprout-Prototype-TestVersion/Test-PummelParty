0172abbaaeb8673fa68fdc74f28f0a6e32f14292
0105kcy <ggbbsing@gmail.com>
2023-05-31 18:13:04 +0900

    [Feature] Add RPC
    RPC 인덱스 증가 함수 호출

---
+++ Prototype-Pummel Party/Assets/RoomManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using Unity.VisualScripting;
+
+public class RoomManager : MonoBehaviourPunCallbacks
+{
+    public PhotonView photonView;
+
+    [SerializeField] private GameObject roomName;
+    [SerializeField] private Transform[] spawnPositions;
+    [SerializeField] private GameObject[] models;
+
+    private bool[] isOccupying = new bool[PhotonNetwork.CurrentRoom.MaxPlayers + 1]; 
+    private TMP_Text roomNameText;
+    private int playerEnterOther = 0;
+
+    private void Awake()
+    {
+        roomNameText = roomName.GetComponent<TMP_Text>();
+    }
+
+
+    public override void OnEnable()
+    {
+        photonView.RPC("PlusOrderNumber", RpcTarget.All);
+        PhotonNetwork.Instantiate(models[playerEnterOther].name, spawnPositions[playerEnterOther].position, Quaternion.identity);
+        roomNameText.text = PhotonNetwork.CurrentRoom.Name;
+    }
+    
+
+    [PunRPC]
+    public void PlusOrderNumber()
+    {
+        playerEnterOther++;
+    }
+  
+}
