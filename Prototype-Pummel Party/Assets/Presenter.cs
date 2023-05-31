9afcfc266517925887f7f688f648559aafe3d13a
0105kcy <ggbbsing@gmail.com>
2023-05-31 16:36:51 +0900

    [Feature] PlayerInstantiate

--- Prototype-Pummel Party/Assets/Presenter.cs
+++ Prototype-Pummel Party/Assets/Presenter.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using Unity.VisualScripting;
 
-public class Presenter : MonoBehaviour
+public class Presenter : MonoBehaviourPunCallbacks
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject roomName;
+    [SerializeField] private Transform[] spawnPositions;
+    [SerializeField] private GameObject[] models;
+    private bool[] isOccupying = new bool[PhotonNetwork.CurrentRoom.MaxPlayers + 1]; 
+    private TextMeshPro roomNameText;
+    private int playerEnterOther = 1;
+
+    private void Awake()
     {
+        roomNameText = roomName.GetComponent<TextMeshPro>();
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Instantiate(models[playerEnterOther], spawnPositions[playerEnterOther].position, Quaternion.identity);
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
