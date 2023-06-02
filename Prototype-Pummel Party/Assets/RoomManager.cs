bd21d5dfa6342927a0f3a69ca0c9cc27f4ed04cb
EunSu <eunning09005@gmail.com>
2023-06-02 19:35:52 +0900

    [Feature] Test Owner

--- Prototype-Pummel Party/Assets/RoomManager.cs
+++ Prototype-Pummel Party/Assets/RoomManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
 using Unity.VisualScripting;
+using Photon.Utilities;
 
 public class RoomManager : MonoBehaviourPunCallbacks, IPunObservable
 {
@@ -13,6 +14,7 @@ public class RoomManager : MonoBehaviourPunCallbacks, IPunObservable
     [SerializeField] private GameObject roomName;
     [SerializeField] private Transform[] spawnPositions;
     [SerializeField] private GameObject[] models;
+    [SerializeField] private GameObject[] buttons;
 
     private GameObject playerMeterial;
     private TMP_Text roomNameText;
@@ -21,7 +23,8 @@ public class RoomManager : MonoBehaviourPunCallbacks, IPunObservable
 
     private void Awake()
     {
-        roomNameText = roomName.GetComponent<TMP_Text>();       
+        roomNameText = roomName.GetComponent<TMP_Text>();
+        photonView = buttons[1].GetPhotonView();
     }
 
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
@@ -38,15 +41,19 @@ public class RoomManager : MonoBehaviourPunCallbacks, IPunObservable
 
     public override void OnJoinedRoom()
     {
+        // 마스터 클라이언트 생성 구간 생성하면서 방장의 정보를 어딘가에 저장해서 보드게임으로 가져갈 필요가 있음
         if (PhotonNetwork.IsMasterClient)
         {
             PhotonNetwork.Instantiate(models[playerEnterOther].name, spawnPositions[playerEnterOther].position, playerRotate);
             playerEnterOther++;
+
+            photonView.RequestOwnership();
         }
 
         roomNameText.text = PhotonNetwork.CurrentRoom.Name;
     }
 
+    // 클라이언트 생성 생성 시 클라이언트의 정보를 어딘가에 저장해서 보드게임으로 가져갈 필요가 있음
     public override void OnPlayerEnteredRoom(Player newPlayer)
     {
         if (PhotonNetwork.IsMasterClient)
@@ -54,10 +61,10 @@ public class RoomManager : MonoBehaviourPunCallbacks, IPunObservable
             PhotonNetwork.Instantiate(models[playerEnterOther].name, spawnPositions[playerEnterOther].position, playerRotate);
             playerEnterOther++;
         }
+
+        //if ()
+        //{
+            
+        //}
     }
-      
-
-
-
-
 }
