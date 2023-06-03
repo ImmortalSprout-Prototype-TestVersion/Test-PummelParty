7ed9b19e58ec29c5b526dd688d3ed5df9eaaf6e0
minyeong <minyk0512@gmail.com>
2023-06-03 19:47:27 +0900

    [Modify] 생성 로직 변경

--- Prototype-Pummel Party/Assets/Scripts/Minigame/MinigameManager.cs
+++ Prototype-Pummel Party/Assets/Scripts/Minigame/MinigameManager.cs
@@ -10,37 +10,44 @@ public class MinigameManager : MonoBehaviour
     [SerializeField] CinemachineVirtualCamera _virtualCamera;
 
     private List<GameObject> _playerPrefabs = new List<GameObject>();
+    private List<int> _playerPrefabId = new List<int>();
 
     private void Awake()
     {
-        // 플레이어 생성하기 -> 이건 각 클라이언트에서 하는 건데 생성되는 거 자체는 연동될 거 같음 (포톤 기능으로)
+        //if(PhotonNetwork.IsMasterClient)
+        //{
+        //    for(int i = 0; i < PhotonNetwork.PlayerList.Length; ++i)
+        //    {
+        //        GameObject newPlayerPrefab = PhotonNetwork.Instantiate("Prefabs/Minigame/BoardgamePlayer", _playerSpawnPosition[i].position, Quaternion.identity);
+        //        newPlayerPrefab.GetPhotonView().TransferOwnership(PhotonNetwork.PlayerList[i]);
+        //        _playerPrefabId.Add(newPlayerPrefab.GetPhotonView().ViewID);
+        //        PhotonNetwork.PlayerList[i].
+        //        // _playerPrefabs.Add(newPlayerPrefab);
+        //    }
+        //}
 
-        // TODO: 마스터한테 스폰 포인트 인덱스 받아오기
+        int num = PhotonNetwork.LocalPlayer.ActorNumber;
+        GameObject newPlayerPrefab = PhotonNetwork.Instantiate("Prefabs/Minigame/BoardgamePlayer", _playerSpawnPosition[num - 1].position, Quaternion.identity);
+        // newPlayerPrefab.GetPhotonView().TransferOwnership(PhotonNetwork.PlayerList[i]);
+        _virtualCamera.LookAt = newPlayerPrefab.transform;
+        _virtualCamera.Follow = newPlayerPrefab.transform;
 
-        // 플레이어 생성하기 프리팹
-        if(PhotonNetwork.IsMasterClient)
-        {
-            for(int i = 0; i < PhotonNetwork.PlayerList.Length; ++i)
-            {
-                GameObject newPlayerPrefab = PhotonNetwork.Instantiate("Prefabs/Minigame/BoardgamePlayer", _playerSpawnPosition[i].position, Quaternion.identity);
-                newPlayerPrefab.GetPhotonView().TransferOwnership(PhotonNetwork.PlayerList[i]);
-                _playerPrefabs.Add(newPlayerPrefab);
-            }
-        }
     }
 
-    private void Start()
-    {
-        for (int i = 0; i < PhotonNetwork.PlayerList.Length; ++i)
-        {
-            PhotonView pv = _playerPrefabs[i].GetComponent<PhotonView>();
+    //private void Start()
+    //{
+    //    for (int i = 0; i < PhotonNetwork.PlayerList.Length; ++i)
+    //    {
+    //        PhotonView pv = _playerPrefabs[i].GetComponent<PhotonView>();
 
-            if (pv.IsMine)
-            {
-                _virtualCamera.LookAt = _playerPrefabs[i].transform;
-                _virtualCamera.Follow = _playerPrefabs[i].transform;
-            }
-        }
-    }
+    //        if (pv.IsMine)
+    //        {
+    //            _virtualCamera.LookAt = _playerPrefabs[i].transform;
+    //            _virtualCamera.Follow = _playerPrefabs[i].transform;
+    //        }
+    //    }
+    //}
+
+    //[]
 
 }
