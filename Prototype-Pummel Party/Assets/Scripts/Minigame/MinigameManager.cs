c1411d560d68bea5eaf944ed07397eda8baf2285
minyeong <minyk0512@gmail.com>
2023-06-03 18:37:35 +0900

    [Feature] 플레이어 오너십 확인

--- Prototype-Pummel Party/Assets/Scripts/Minigame/MinigameManager.cs
+++ Prototype-Pummel Party/Assets/Scripts/Minigame/MinigameManager.cs
@@ -5,6 +5,8 @@ using Photon.Pun;
 
 public class MinigameManager : MonoBehaviour
 {
+    [SerializeField] private Transform[] _playerSpawnPosition;
+
     private void Awake()
     {
         // 플레이어 생성하기 -> 이건 각 클라이언트에서 하는 건데 생성되는 거 자체는 연동될 거 같음 (포톤 기능으로)
@@ -12,6 +14,14 @@ public class MinigameManager : MonoBehaviour
         // TODO: 마스터한테 스폰 포인트 인덱스 받아오기
 
         // 플레이어 생성하기 프리팹
-        PhotonNetwork.Instantiate("Prefabs/Minigame/BoardgamePlayer", transform.position, Quaternion.identity);
+        if(PhotonNetwork.IsMasterClient)
+        {
+            for(int i = 0; i < PhotonNetwork.PlayerList.Length; ++i)
+            {
+                GameObject newPlayerPrefab = PhotonNetwork.Instantiate("Prefabs/Minigame/BoardgamePlayer", transform.position, Quaternion.identity);
+                newPlayerPrefab.GetPhotonView().TransferOwnership(PhotonNetwork.PlayerList[i]);
+            }
+        }
     }
+
 }
